a10f79a282fdda818faa01b89171d842178916f3
cmrohityadav <cmrohityadav23@gmail.com>
2024-12-21 01:26:51 +0530

    aaded all tasks to local store of browser

--- Projects/11_TODO/script.js
+++ Projects/11_TODO/script.js
@@ -1,20 +1,50 @@
-const todoInput=document.getElementById("todo-input")
 
 
-const addTaskBtn=document.getElementById("add-task-btn")
-const todoList=document.getElementById("todo-list")
-const tasksArray=[];
-addTaskBtn.addEventListener("click",()=>{
-    let taskText=todoInput.value.trim();
-   
-    const objTask={
-        id:Date.now(),
-        text:taskText,
-        isCompleted:false
 
+
+document.addEventListener("DOMContentLoaded",function(){
+    const todoInput=document.getElementById("todo-input")
+    const addTaskBtn=document.getElementById("add-task-btn")
+    const todoList=document.getElementById("todo-list")
+    let tasksArray = [];
+    try {
+        const storedTasks = localStorage.getItem("tasksLocalStore");
+        tasksArray = storedTasks ? JSON.parse(storedTasks) : [];
+    } catch (e) {
+        console.error("Error parsing localStorage data:", e);
+        tasksArray = []; // Reset to an empty array if parsing fails
+    }
+    tasksArray.forEach(eachTaskItem=>renderTask(eachTaskItem));
+
+    addTaskBtn.addEventListener("click",()=>{
+        let taskText=todoInput.value.trim();
+        if(taskText==="") return;
+        const objTask={
+            id:Date.now(),
+            text:taskText,
+            isCompleted:false,
+    
+        }
+    
+        tasksArray.push(objTask);
+        saveDataToLocal(tasksArray)
+        
+        todoInput.value=""
+        console.log(tasksArray)
+
+    
+    }) 
+    
+
+    const saveDataToLocal=(tasksArray)=>{
+        localStorage.setItem("tasksLocalStore",JSON.stringify(tasksArray));
     }
 
-    tasksArray.push(objTask);
-    console.log(tasksArray)
+    function renderTask(task) {
+        console.log(task)
+    }
+    
+
+
 
 })
